2a8d76c0a51cdb95606f4e2b1e7dcf02ae3ef030
BS-Joy <badhansamadder5@gmail.com>
2025-05-09 21:03:33 +0600

    project initialized and installed shadcn

---
+++ app/(admin)/admin/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <p>This is admin page</p>
+      <Button>Go To Home Page</Button>
+    </div>
+  );
+};
+
+export default Page;
