fc4ab05b1eb6d92915e203ae055f88c70b743948
BS-Joy <badhansamadder5@gmail.com>
2025-05-12 01:47:25 +0600

    added sidebar and navbar

--- app/(admin)/admin/page.js
+++ app/(admin)/admin/page.js
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
       <p>This is admin page</p>
-      <Button>Go To Home Page</Button>
+      <Link href="/" className={buttonVariants({ variant: "default" })}>
+        Go To Home Page
+      </Link>
     </div>
   );
 };
