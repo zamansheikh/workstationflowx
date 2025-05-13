49e335bef2e2e1c13bb5fa0de00da397b1fcd6b6
BS-Joy <badhansamadder5@gmail.com>
2025-05-13 19:22:48 +0600

    checking

---
+++ app/(admin)/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Workflow_X | Admin",
+  description: "This is admin panel",
+};
+
+export default function RootLayout({ children }) {
+  return <>{children}</>;
+}
